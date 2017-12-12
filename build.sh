ac33a599e8a032e30e1032c2938f138115ae157a
Yri Davies <0xdavy@gmail.com>
2017-12-12 14:52:19 +0100

    Restructure Entertainment menu

--- build.sh
+++ build.sh
@@ -23,10 +23,10 @@ dev=(
 entertainment=(
     "Reddit|http://reddit.com"
     "YouTube|http://youtube.com"
+    "UnixSPACEPorn|http://unixporn.reddit.com"
     "Let'sSPACEPlay|https://www.youtube.com/user/LetsPlay/videos"
     "AchievementSPACEHunter|https://www.youtube.com/user/AchievementHunter/videos"
     "ArtisanSPACEVideos|http://artisanvideos.reddit.com"
-    "UnixSPACEPorn|http://unixporn.reddit.com"
 )
 news=(
     "Bloomberg|http://bloomberg.com"
